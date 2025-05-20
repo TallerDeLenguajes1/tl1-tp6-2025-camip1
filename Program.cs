4540ca9ad6fd807c4ec2e63fe412361da4ba1a19
Perez Milagros Camila <camilap11517@gmail.com>
2025-05-20 13:13:08 -0300

    commit ejercicio 1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Runtime.InteropServices;
 
+/*
 Console.WriteLine("Hello, World!");
 
 int a;
@@ -9,5 +10,34 @@ a=10;
 b=a;
 Console.WriteLine("valor de a:"+a);
 Console.WriteLine("valor de b:" + b);
+*/
+
+//ejercicio 1 invertir numero
+/*int n;
+string s;
+
+do{
+    Console.WriteLine("Ingrese un entero mayor a cero");
+    s = Console.ReadLine();
+} while (!int.TryParse(s, out n));
+
+Console.WriteLine("El numero ingresado es "+n);
+
+if (n > 0)
+{
+    int dig;
+    int invertido = 0;
+    while (n > 0)
+    {
+        dig = n % 10;
+        invertido = invertido * 10 + dig;
+        n = n / 10;
+    }
+    Console.WriteLine("El numero invertido es:" + invertido);
+}
+else
+{
+    Console.WriteLine("El numero debe ser mayor a 0");
+}*/
 
 
