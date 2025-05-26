428ca68009eefeec4095445d50e2b8f7e91a0fc3
Perez Milagros Camila <camilap11517@gmail.com>
2025-05-26 12:59:05 -0300

    commit ejercicio 2: calculadora v1 con operaciones basicas

--- Program.cs
+++ Program.cs
@@ -40,4 +40,48 @@ else
     Console.WriteLine("El numero debe ser mayor a 0");
 }*/
 
+//ejercicio 2 calculadora V1
+int op;
+do {
+    Console.WriteLine("Menu:\nopcion 1: Sumar\nopcion 2: Restar\nopcion 3: Multiplicar\nopcion 4: Dividir\nopcion elegida:");
+    string s = Console.ReadLine();
+    op = Convert.ToInt32(s);
 
+    double num1;
+    double num2;
+
+    Console.WriteLine("operando 1:");
+    string entrada = Console.ReadLine();
+    bool esvalidonum1 = double.TryParse(entrada, out num1);
+    
+    Console.WriteLine("operando 2:");
+    entrada=Console.ReadLine();
+    bool esvalidonum2 = double.TryParse(entrada, out num2);
+
+    if (esvalidonum1 && esvalidonum2 && !(op==4 && num2==0))
+    {
+        switch (op)
+        {
+            case 1:
+                Console.WriteLine("Resultado=" + (num1 + num2));
+                break;
+            case 2:
+                Console.WriteLine("Resultado=" + (num1 - num2));
+                break;
+            case 3:
+                Console.WriteLine("Resultado=" + (num1 * num2));
+                break;
+            case 4:
+                Console.WriteLine("Resultado=" + (num1 / num2));
+                break;
+            default:
+                Console.WriteLine("Opcion invalida!");
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Error");
+    }
+    Console.WriteLine("¿Desea realizar otro calculo?: si(marque 1)/no(marque 0)");
+} while (Convert.ToInt32(Console.ReadLine())==1);
