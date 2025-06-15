5b38885d75502e833c5c54c9f71e3672f3256959
Perez Milagros Camila <camilap11517@gmail.com>
2025-06-15 18:02:03 -0300

    modificacion calculadora v2

--- Program.cs
+++ Program.cs
@@ -41,52 +41,6 @@ else
     Console.WriteLine("El numero debe ser mayor a 0");
 }*/
 
-//ejercicio 2 calculadora V1
-int op;
-string s, entrada;
-bool esvalidonum1, esvalidonum2;
-
-do {
-    Console.WriteLine("Menu:\nopcion 1: Sumar\nopcion 2: Restar\nopcion 3: Multiplicar\nopcion 4: Dividir\nopcion elegida:");
-    s = Console.ReadLine();
-    op = Convert.ToInt32(s);
-
-    Console.WriteLine("operando 1:");
-    entrada = Console.ReadLine();
-    esvalidonum1 = double.TryParse(entrada, out double operando1);
-
-    Console.WriteLine("operando 2:");
-    entrada =Console.ReadLine();
-    esvalidonum2 = double.TryParse(entrada, out double operando2);
-
-    if (esvalidonum1 && esvalidonum2 && !(op==4 && operando2==0))
-    {
-        switch (op)
-        {
-            case 1:
-                Console.WriteLine("Resultado=" + (operando1 + operando2));
-                break;
-            case 2:
-                Console.WriteLine("Resultado=" + (operando1 - operando2));
-                break;
-            case 3:
-                Console.WriteLine("Resultado=" + (operando1 * operando2));
-                break;
-            case 4:
-                Console.WriteLine("Resultado=" + (operando1 / operando2));
-                break;
-            default:
-                Console.WriteLine("Opcion invalida!");
-                break;
-        }
-    }
-    else
-    {
-        Console.WriteLine("Error");
-    }
-    Console.WriteLine("¿Desea realizar otro calculo?: si(marque 1)/no(marque 0)");
-} while (Convert.ToInt32(Console.ReadLine())==1);
-
 //ejercicio 3
 string input;
 bool esNumValido;
