6695f2369ea3cb980668ff869fc2a91847f4b0b8
Perez Milagros Camila <camilap11517@gmail.com>
2025-06-15 18:14:35 -0300

    modificacion calculadora v1

--- Program.cs
+++ Program.cs
@@ -42,37 +42,37 @@ else
 
 //ejercicio 2 calculadora V1
 int op;
+string s, entrada;
+bool esvalidonum1, esvalidonum2;
+
 do {
     Console.WriteLine("Menu:\nopcion 1: Sumar\nopcion 2: Restar\nopcion 3: Multiplicar\nopcion 4: Dividir\nopcion elegida:");
-    string s = Console.ReadLine();
+    s = Console.ReadLine();
     op = Convert.ToInt32(s);
 
-    double num1;
-    double num2;
-
     Console.WriteLine("operando 1:");
-    string entrada = Console.ReadLine();
-    bool esvalidonum1 = double.TryParse(entrada, out num1);
-    
-    Console.WriteLine("operando 2:");
-    entrada=Console.ReadLine();
-    bool esvalidonum2 = double.TryParse(entrada, out num2);
+    entrada = Console.ReadLine();
+    esvalidonum1 = double.TryParse(entrada, out double operando1);
 
-    if (esvalidonum1 && esvalidonum2 && !(op==4 && num2==0))
+    Console.WriteLine("operando 2:");
+    entrada =Console.ReadLine();
+    esvalidonum2 = double.TryParse(entrada, out double operando2);
+
+    if (esvalidonum1 && esvalidonum2 && !(op==4 && operando2==0))
     {
         switch (op)
         {
             case 1:
-                Console.WriteLine("Resultado=" + (num1 + num2));
+                Console.WriteLine("Resultado=" + (operando1 + operando2));
                 break;
             case 2:
-                Console.WriteLine("Resultado=" + (num1 - num2));
+                Console.WriteLine("Resultado=" + (operando1 - operando2));
                 break;
             case 3:
-                Console.WriteLine("Resultado=" + (num1 * num2));
+                Console.WriteLine("Resultado=" + (operando1 * operando2));
                 break;
             case 4:
-                Console.WriteLine("Resultado=" + (num1 / num2));
+                Console.WriteLine("Resultado=" + (operando1 / operando2));
                 break;
             default:
                 Console.WriteLine("Opcion invalida!");
@@ -85,3 +85,4 @@ do {
     }
     Console.WriteLine("¿Desea realizar otro calculo?: si(marque 1)/no(marque 0)");
 } while (Convert.ToInt32(Console.ReadLine())==1);
+
