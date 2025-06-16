3c4b2aa18aa3dd336c18af407c5ea4e4773606ad
Perez Milagros Camila <camilap11517@gmail.com>
2025-06-15 21:18:54 -0300

    ejercicio 4 completo

--- Program.cs
+++ Program.cs
@@ -40,104 +40,196 @@ else
     Console.WriteLine("El numero debe ser mayor a 0");
 }*/
 
+//--------------------------------------------------------------------------------------------------
 //ejercicio 2 calculadora V1
-int op;
-string s, entrada;
-bool esvalidonum1, esvalidonum2;
 
-do {
-    Console.WriteLine("Menu:\nopcion 1: Sumar\nopcion 2: Restar\nopcion 3: Multiplicar\nopcion 4: Dividir\nopcion elegida:");
-    s = Console.ReadLine();
-    op = Convert.ToInt32(s);
+// int op;
+// string s, entrada;
+// bool esvalidonum1, esvalidonum2;
+
+// do {
+//     Console.WriteLine("Menu:\nopcion 1: Sumar\nopcion 2: Restar\nopcion 3: Multiplicar\nopcion 4: Dividir\nopcion elegida:");
+//     s = Console.ReadLine();
+//     op = Convert.ToInt32(s);
     
-    Console.WriteLine("operando 1:");
-    entrada = Console.ReadLine();
-    esvalidonum1 = double.TryParse(entrada, out double operando1);
+//     Console.WriteLine("operando 1:");
+//     entrada = Console.ReadLine();
+//     esvalidonum1 = double.TryParse(entrada, out double operando1);
 
-    Console.WriteLine("operando 2:");
-    entrada = Console.ReadLine();
-    esvalidonum2 = double.TryParse(entrada, out double operando2);
+//     Console.WriteLine("operando 2:");
+//     entrada = Console.ReadLine();
+//     esvalidonum2 = double.TryParse(entrada, out double operando2);
 
-    if (esvalidonum1 && esvalidonum2 && !(op == 4 && operando2 == 0))
-    {
-        switch (op)
-        {
-            case 1:
-                Console.WriteLine("Resultado=" + (operando1 + operando2));
-                break;
-            case 2:
-                Console.WriteLine("Resultado=" + (operando1 - operando2));
-                break;
-            case 3:
-                Console.WriteLine("Resultado=" + (operando1 * operando2));
-                break;
-            case 4:
-                Console.WriteLine("Resultado=" + (operando1 / operando2));
-                break;
-            default:
-                Console.WriteLine("Opcion invalida!");
-                break;
-        }
-    }
-    else
-    {
-        Console.WriteLine("Error");
-    }
-    Console.WriteLine("¿Desea realizar otro calculo?: si(marque 1)/no(marque 0)");
-} while (Convert.ToInt32(Console.ReadLine()) == 1);
+//     if (esvalidonum1 && esvalidonum2 && !(op == 4 && operando2 == 0))
+//     {
+//         switch (op)
+//         {
+//             case 1:
+//                 double suma = operando1 + operando2;
+//                 Console.WriteLine("La suma de " + operando1.ToString() + " y de " + operando2.ToString() + " es " + suma.ToString());
+//                 break;
+//             case 2:
+//                 double resta = operando1 - operando2;
+//                 Console.WriteLine("La diferencia entre " + operando1.ToString() + " y " + operando2.ToString() + " es " + resta.ToString());
+//                 break;
+//             case 3:            
+//                 double producto = operando1 * operando2;
+//                 Console.WriteLine("El producto de " + operando1.ToString() + " por " + operando2.ToString() + " es " + producto.ToString());
+//                 break;
+//             case 4:
+//                 if (operando2 != 0)
+//                 {
+//                     double cociente = operando1 / operando2;
+//                     Console.WriteLine("El cocientre entre " + operando1.ToString() + " y " + operando2.ToString() + " es " + cociente.ToString());
+//                 }
+//                 else
+//                 {
+//                     Console.WriteLine("Error: division por cero");
+//                 }
+//                 break;
+//             default:
+//                 Console.WriteLine("Opcion invalida!");
+//                 break;
+//         }
+//     }
+//     Console.WriteLine("¿Desea realizar otro calculo?: si(marque 1)/no(marque 0)");
+// } while (Convert.ToInt32(Console.ReadLine()) == 1);
 
+
+//--------------------------------------------------------------------------------------------------
 //ejercicio 3
-string input;
-bool esNumValido;
-double numero;
-do
+// string input;
+// bool esNumValido;
+// double numero;
+// do
+// {
+//     Console.WriteLine("Ingrese un numero:");
+//     input = Console.ReadLine();
+//     esNumValido = double.TryParse(input, out numero);
+//     if (!esNumValido)
+//     {
+//         Console.WriteLine("Numero invalido!");
+//     }
+// } while (!esNumValido);
+
+// double valorAbs = Math.Abs(numero);
+// double cuadrado = numero*numero;
+// double raizCuadrada = Math.Sqrt(numero);
+// double cos = Math.Cos(numero);
+// double sin = Math.Sin(numero);
+// int parteEntera = (int)numero;
+
+// Console.WriteLine($"El valor absoluto de {numero} es:{valorAbs}");
+// Console.WriteLine($"El cuadrado del numero ingresado es:{cuadrado:F2}");
+// Console.WriteLine($"La raiz cuadrada del numero ingresado es:{raizCuadrada:F2}");
+// Console.WriteLine($"El coseno del numero ingresado (en radianes) es:{cos:F4}");
+// Console.WriteLine($"El seno del numero ingresado (en radianes) es:{sin:F4}");
+// Console.WriteLine($"La parte entera del numero ingresado es:{parteEntera}");
+
+// Console.WriteLine("Ingrese un numero:");
+// input = Console.ReadLine();
+// esvalidonum1 = double.TryParse(input, out double num1);
+
+// Console.WriteLine("Ingrese otro numero:");    
+// input = Console.ReadLine();
+// esvalidonum2= double.TryParse(input, out double num2);
+
+// if (esvalidonum1 && esvalidonum2){
+//     if (num1 > num2){
+//         Console.WriteLine($"{num1} es el maximo");
+//         Console.WriteLine($"{num2} es el minimo");
+//     }
+//     else if (num2 > num1){
+//         Console.WriteLine($"{num2} es el maximo");
+//         Console.WriteLine($"{num1} es el minimo");
+//     }
+//     else{
+//         Console.WriteLine("Los numeros ingresados son iguales. No hay maximo ni minimo");
+//     }
+// }
+// else{
+//     Console.WriteLine("Se ingresan numeros invalidos");
+// }
+
+
+
+//ejercicio 4
+// string cadena1, cadena2;
+
+// Console.WriteLine("Ingrese una cadena:");
+// cadena1 = Console.ReadLine();
+// Console.WriteLine($"La cadena ingresada es: {cadena1}");
+
+// int longitud = cadena1.Length;
+// Console.WriteLine($"Longitud:{longitud}");
+
+// Console.WriteLine("\nIngrese otra cadena:");
+// cadena2 = Console.ReadLine();
+// Console.WriteLine($"La cadena ingresada es: {cadena2}");
+
+// string concatenada = String.Concat(cadena1, cadena2);
+// Console.WriteLine($"Cadenas concatenadas: {concatenada}");
+
+// string subcadena = cadena1.Substring(1, 3);
+// Console.WriteLine($"Una subcadena de \"{cadena1}\" es \"{subcadena}\"");
+
+// Console.WriteLine("Elementos de la cadena:");
+// foreach (char letra in cadena1)
+// {
+//     Console.WriteLine(letra);
+// }
+
+// Console.WriteLine("Ingrese la palabra a buscar en la cadena:");
+// string palabra = Console.ReadLine();
+// int indice = cadena1.IndexOf(palabra);
+// if (indice != -1)
+// {
+//     Console.WriteLine($"La palabra \"{palabra}\" se encuentra en la posicion {indice} de la cadena \"{cadena1}\"");
+// }
+// else
+// {
+//     Console.WriteLine($"No se encontro la palabra {palabra} en la cadena ingresada");
+// }
+
+// string mayuscula = cadena1.ToUpper();
+// Console.WriteLine($"Cadena en mayusculas: {mayuscula}");
+// Console.WriteLine($"Cadena en minuscula: {mayuscula.ToLower()}");
+
+// Console.WriteLine("Ingrese un texto:");
+// string texto = Console.ReadLine();
+// string[] palabras = texto.Split(" ");
+// foreach (string elemento in palabras)
+// {
+//     Console.WriteLine(elemento);
+// }
+
+
+Console.WriteLine("Ingrese una ecuacion simple (por ejemplo, 582 + 2): ");
+string ecuacion = Console.ReadLine();
+string[] elementos = ecuacion.Split(" ");
+
+if (int.TryParse(elementos[0], out int a) && int.TryParse(elementos[2], out int b))
 {
-    Console.WriteLine("Ingrese un numero:");
-    input = Console.ReadLine();
-    esNumValido = double.TryParse(input, out numero);
-    if (!esNumValido)
+    switch (elementos[1]) //operador
     {
-        Console.WriteLine("Numero invalido!");
-    }
-} while (!esNumValido);
-
-double valorAbs = Math.Abs(numero);
-double cuadrado = numero*numero;
-double raizCuadrada = Math.Sqrt(numero);
-double cos = Math.Cos(numero);
-double sin = Math.Sin(numero);
-int parteEntera = (int)numero;
-
-Console.WriteLine($"El valor absoluto de {numero} es:{valorAbs}");
-Console.WriteLine($"El cuadrado del numero ingresado es:{cuadrado:F2}");
-Console.WriteLine($"La raiz cuadrada del numero ingresado es:{raizCuadrada:F2}");
-Console.WriteLine($"El coseno del numero ingresado (en radianes) es:{cos:F4}");
-Console.WriteLine($"El seno del numero ingresado (en radianes) es:{sin:F4}");
-Console.WriteLine($"La parte entera del numero ingresado es:{parteEntera}");
-
-Console.WriteLine("Ingrese un numero:");
-input = Console.ReadLine();
-esvalidonum1 = double.TryParse(input, out double num1);
-
-Console.WriteLine("Ingrese otro numero:");    
-input = Console.ReadLine();
-esvalidonum2= double.TryParse(input, out double num2);
-
-if (esvalidonum1 && esvalidonum2){
-    if (num1 > num2){
-        Console.WriteLine($"{num1} es el maximo");
-        Console.WriteLine($"{num2} es el minimo");
-    }
-    else if (num2 > num1){
-        Console.WriteLine($"{num2} es el maximo");
-        Console.WriteLine($"{num1} es el minimo");
-    }
-    else{
-        Console.WriteLine("Los numeros ingresados son iguales. No hay maximo ni minimo");
+        case "+":
+            Console.WriteLine($"La suma de {a} y {b} es {a + b}");
+            break;
+        case "-":
+            Console.WriteLine($"La diferencia entre {a} y {b} es {a - b}");
+            break;
+        case "*":
+            Console.WriteLine($"El producto de {a} por {b} es {a * b}");
+            break;
+        case "/":
+            if (b != 0)
+            {
+                Console.WriteLine($"EL cociente entre {a} y {b} es {a / b}");
+            }
+            else
+            {
+                Console.WriteLine("Error. Division por cero");
+            }
+            break;
     }
 }
-else{
-    Console.WriteLine("Se ingresan numeros invalidos");
-}
-
-
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
+# Respuestas ejercicio 4
+
+## ¿String es una tipo por valor o un tipo por referencia?
+En **C#**, la cadena (string) es un tipo de referencia, aunque se comporte de manera similar a un tipo de valor en muchos casos. 
+
+En C#, string es una clase que representa una secuencia de caracteres Unicode. string es un alias para la clase System.String. La clase string es de tipo referencia, lo que significa que las variables de tipo cadena almacenan una referencia a un objeto en la memoria, en lugar de almacenar directamente el valor de la cadena. 
+
+## ¿Qué secuencias de escape tiene el tipo string?
+En el tipo string, las secuencias de escape se utilizan para representar caracteres especiales que no pueden ser introducidos directamente en una cadena. Estas secuencias comienzan con una barra invertida **(\@)** seguida de un carácter específico, que indica la acción a realizar. 
+Algunas secuencias de escape son: 
+\@\: Representa una única barra invertida.
+\': Representa una comilla simple.
+\": Representa una comilla doble.
+\n: Representa una nueva línea.
+\r: Representa un retorno de carro.
+\t: Representa una tabulación horizontal.
+\b: Representa un retroceso.
+\f: Representa un salto de página.
+\v: Representa una tabulación vertical.
+\a: Representa una alerta o timbre.
+\0: Representa el carácter nulo.
+
+## ¿Qué sucede cuando utiliza el carácter @ y $ antes de una cadena de texto?
+**@ (Literal String):**
+Cuando se utiliza antes de una cadena, el carácter **@** indica que la cadena es un literal de cadena. Esto significa que las secuencias de escape (como \n para nueva línea o \t para tabulación) no se interpretarán como tales y se tratarán literalmente, incluyendo las barras invertidas. 
+
+**$ (String Interpolation):**
+El carácter $ indica que la cadena es una cadena interpolada. Esto permite insertar expresiones dentro de la cadena, que serán evaluadas y convertidas a texto en tiempo de ejecución.
+
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/24pP-Pw_)
